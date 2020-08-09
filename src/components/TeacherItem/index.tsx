c164fe2aa1ae4e4a005b16dbc1b5a89f9d78631b
danielferreiradf <danielferreiradk@gmail.com>
2020-08-09 15:29:54 +0100

    Added function to TeacherList page

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,33 +4,35 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://store.playstation.com/store/api/chihiro/00_09_000/container/GB/en/999/EP0149-CUSA09988_00-AV00000000000001/1553560094000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000"
-          alt="Avatar"
-        />
+        <img src={teacher.avatar} alt="Avatar" />
         <div>
-          <strong>Daniel Ferreira</strong>
-          <span>Programação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
-        tenetur! <br /> <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-        beatae totam ut eligendi dolorum porro, eos, illum doloremque unde
-        placeat aliquam laudantium vero quod? Excepturi reprehenderit
-        consequatur laborum molestiae accusantium nam aperiam, est aliquam minus
-        eius sapiente fuga at facere!
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 30,00</strong>
+          <strong>{teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp" />
